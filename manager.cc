4ef6b9da271f25c3b69d11446063402bed5cb98d
Nathan Barteaux <ndbarteaux@gmail.com>
2017-11-14 00:28:02 -0700

    Can get connectivity table now

--- manager.cc
+++ manager.cc
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <iostream> 
 #include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include<vector>
 #include <sys/wait.h> 
 #include <unistd.h>
 #include <fstream>
@@ -11,32 +14,37 @@
 #include "project3.h"
 
 using namespace std;
+int children;
 
-
-void parentFunction(int fd) {
-	/*
-	int pid = getppid();
-	char shmString[12];
-	sprintf(shmString, "Manager: Hello World!);
-	write(fd, shmString, sizeof(shmString));
-	*/
-}
-
-
-int main(int argc, char * argv[]) {
+void parentFunction() {
 	int fd, children;
+	vector<string> vec;
 	fd = open("test", O_CREAT|O_TRUNC|O_WRONLY, 0666);
 	
 	string line;
+	int i = 0;
 	ifstream numChildren ("input");
 	if(numChildren.is_open()) {
 		while(getline(numChildren, line)) {
-			istringstream ss(line);
-			// Children holds how many forks to spawn, read from file.
-			ss >> children;					
+			if(line.compare("-1") == 1)
+				break;
+			if (i = 0) {
+				istringstream ss(line);
+				// Children holds how many forks to spawn, read from file. Always first line
+				ss >> children;	
+			}
+			i=1;
+			stringstream sstream;
+			sstream << line;
+			string s = sstream.str();
+			vec.push_back(s);
 		}
 	}
-	
+	vec.pop_back();
+	for(auto v : vec) {
+		cout << v << "\n" << endl;
+	}
+
 	// As many pid's as children
 	pid_t pid[children];	
 	// Spawn Children
@@ -46,10 +54,17 @@ int main(int argc, char * argv[]) {
 		} else if(pid[i] == 0) {
 			// Have child do something (write to file for now)
 			childFunction(fd);
-			exit(0);
 		}
-	}
-	
+	} 
+}
+
+
+int main(int argc, char * argv[]) {
+	if(argc == 1) {
+		fprintf(stderr, "Usage for %s: $./manager <input>", argv[0]);
+		exit(1);
+	} 
+	parentFunction();
 	// Wait for children to exit
 	int status;
 	// For each specific child
--- project3.h
+++ project3.h
@@ -1,7 +1,7 @@
 #ifndef P3_H
 #define P3_H
 
-void parentFunction(int fd);
+void parentFunction();
 void childFunction(int fd);
 
 #endif
