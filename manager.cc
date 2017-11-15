9e69aad95436f282596390174f81a844389ebb69
Ian <ianaf100@gmail.com>
2017-11-15 01:01:15 -0700

    Smoothed out some of the forking issues

--- manager.cc
+++ manager.cc
@@ -5,7 +5,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include<vector>
-#include <sys/wait.h> 
+#include <sys/wait.h>
 #include <unistd.h>
 #include <fstream>
 #include <fcntl.h>
@@ -18,50 +18,48 @@ int children;
 
 void parentFunction() {
 	int fd, children;
-	vector<string> vec;
-	fd = open("test", O_CREAT|O_TRUNC|O_WRONLY, 0666);
-	
 	string line;
-	int i = 0;
-	ifstream numChildren ("input");
-	if(numChildren.is_open()) {
-		while(getline(numChildren, line)) {
-			if(line.compare("-1") == 1)
-				break;
-			if (i = 0) {
-				istringstream ss(line);
-				// Children holds how many forks to spawn, read from file. Always first line
-				ss >> children;	
-			}
-			i=1;
+	vector<string> lines;
+	fd = open("test.txt", O_CREAT|O_TRUNC|O_WRONLY, 0666);
+	ifstream input ("input.txt");
+
+	if(input.is_open()) {
+
+		// children holds how many forks to spawn, read from first line of file
+		getline(input, line);
+		istringstream numChildren(line);
+		numChildren >> children;
+
+		while(getline(input, line)) {
 			stringstream sstream;
 			sstream << line;
 			string s = sstream.str();
-			vec.push_back(s);
+			lines.push_back(s);
 		}
 	}
-	vec.pop_back();
-	for(auto v : vec) {
-		cout << v << "\n" << endl;
-	}
 
-	// As many pid's as children
+	lines.pop_back();
+	for(auto v : lines) {
+		cout << v << "\n" << endl;           
+	}                                        
+                                             
+	// As many pid's as children             
 	pid_t pid[children];	
 	// Spawn Children
 	for(int i=0; i<children; i++) {
 		if ((pid[i] = fork()) < 0) {
 			perror("Fork failed");
 		} else if(pid[i] == 0) {
-			// Have child do something (write to file for now)
-			childFunction(fd);
+			childFunction(fd);  // have child do something (write to file for now)
+			exit(0); // so childs don't continue to fork
 		}
-	} 
+	}
 }
 
 
 int main(int argc, char * argv[]) {
 	if(argc == 1) {
-		fprintf(stderr, "Usage for %s: $./manager <input>", argv[0]);
+		fprintf(stderr, "Usage for %s: $./manager <input.txt>", argv[0]);
 		exit(1);
 	} 
 	parentFunction();
@@ -69,8 +67,7 @@ int main(int argc, char * argv[]) {
 	int status;
 	// For each specific child
 	pid_t pidChild;
-	while(children > 0) {
-		pidChild = wait(&status);
+	while ((pidChild = wait(&status)) > 0) {
 		cout << "Child with PID " << pidChild << " exited with status of " << status << endl;
 		children--;
 	}
